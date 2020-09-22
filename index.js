2b06dc0ae66097b60dc3afa438a32a2380480aa5
Nicolas <nicolas@asphalt.run>
2020-09-22 12:41:51 +0200

    add ddba and event

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const _data=require('./lib/data.js');
 const _string=require('./lib/string.js');
 const _number=require('./lib/number.js');
+const _ddba=require('./lib/ddba.js');
+const _event=require('./lib/event.js');
 
 module.exports={
 	DataHandler:_data,
 	StringHandler:_string,
 	NumberHandler:_number,
-	Core:{..._data,..._string,..._number}
+	ddba:_ddba,
+	Event:_event,
+	Core:{..._data,..._string,..._number,..._event}
 }
