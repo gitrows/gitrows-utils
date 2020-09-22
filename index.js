75d2d9a3fd650bab3a2d31d6f1fdb0a19e1a9f13
Nicolas <nicolas@asphalt.run>
2020-09-22 12:18:12 +0200

    add speed conversion

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const _data=require('./lib/data.js');
 const _string=require('./lib/string.js');
+const _number=require('./lib/number.js');
 
 module.exports={
 	DataHandler:_data,
 	StringHandler:_string,
-	Core:{..._data,..._string}
+	NumberHandler:_number,
+	Core:{..._data,..._string,..._number}
 }
