01bb0f47dabae9f12b95461065b9307f14b2e31b
Nicolas <nicolas@asphalt.run>
2020-10-06 14:45:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const _string=require('./lib/string.js');
 const _number=require('./lib/number.js');
 const _ddba=require('./lib/ddba.js');
 const _event=require('./lib/event.js');
+const _s3=require('./lib/s3.js');
 
 module.exports={
 	DataHandler:_data,
@@ -10,5 +11,6 @@ module.exports={
 	NumberHandler:_number,
 	ddba:_ddba,
 	Event:_event,
+	S3:_s3,
 	Core:{..._data,..._string,..._number,..._event}
 }
